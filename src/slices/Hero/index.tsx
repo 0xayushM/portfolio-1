75f41fc7a7eda5ed8473077dbe9b4725e6c51bfb
Ayush Mangal <mangal.ayush.4982@gmail.com>
2024-02-01 17:08:35 +0530

    added gsap animation to name and tagline

--- src/slices/Hero/index.tsx
+++ src/slices/Hero/index.tsx
@@ -1,5 +1,8 @@
+"use client"
 import { Content, KeyTextField } from "@prismicio/client";
 import { SliceComponentProps } from "@prismicio/react";
+import { useContext, useEffect, useRef } from "react";
+import gsap from "gsap";
 
 /**
  * Props for `Hero`.
@@ -10,6 +13,46 @@ export type HeroProps = SliceComponentProps<Content.HeroSlice>;
  * Component for "Hero" Slices.
  */
 const Hero = ({ slice }: HeroProps): JSX.Element => {
+
+  const component = useRef(null)
+  // letter animation using gsap
+  useEffect(() => {
+    let ctx = gsap.context(()=>{
+      const tl = gsap.timeline()
+      tl.fromTo(
+        ".name-animation",
+        {
+          x:-100,
+          opacity:0,
+          rotate:-10
+        },
+        {
+          x:0,
+          opacity:1,
+          rotate:0,
+          ease:'elastic.out(1,0.3)',
+          duration: 1,
+          transformOrigin:'leftTop',
+          stagger : {each:0.1}
+        }
+      )
+      tl.fromTo(
+        ".job-title",
+        {
+          y:20, opacity:0, scale:1.2,
+        },
+        {
+          opacity:1,
+          y:0,
+          duration:1,
+          delay:0.5,
+          scale:1,
+          ease:"elastic.out(1,0.3)"
+        }
+      )
+    }, component)
+    return () => ctx.revert()
+  }, [])
 
   // function which splits the letters into spans for animation effects
   const renderLetters = (name: KeyTextField, key: string): JSX.Element[] | null => {
@@ -26,6 +69,7 @@ const Hero = ({ slice }: HeroProps): JSX.Element => {
     <section
       data-slice-type={slice.slice_type}
       data-slice-variation={slice.variation}
+      ref={component}
     >
       <div className="grid min-h-[70vh] grid-cols-1 md:grid-cols-2 items-center">
         <div className="col-start-1 md:row-start-1">
@@ -39,7 +83,7 @@ const Hero = ({ slice }: HeroProps): JSX.Element => {
               {renderLetters(slice.primary.last_name, "last")}
             </span>
           </h1>
-          <span className="block bg-gradient-to-tr from-yellow-500 via-yellow-200 to-yellow-500 bg-clip-text text-2xl font-bold uppercase tracking-[.2em] text-transparent opacity-1 md:text-4xl">{slice.primary.tag_line}</span>
+          <span className="job-title block bg-gradient-to-tr from-yellow-500 via-yellow-200 to-yellow-500 bg-clip-text text-2xl font-bold uppercase tracking-[.2em] text-transparent opacity-0 md:text-4xl">{slice.primary.tag_line}</span>
         </div>
       </div>
     </section>
