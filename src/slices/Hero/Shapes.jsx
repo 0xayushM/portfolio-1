7c61e228f3ecef076d068fdbcf59051c671c74cb
Ayush Mangal <mangal.ayush.4982@gmail.com>
2024-02-02 20:45:33 +0530

    added materials

--- src/slices/Hero/Shapes.jsx
+++ src/slices/Hero/Shapes.jsx
@@ -29,7 +29,14 @@ function Geometries() {
         },
     ]
     const materials = [
-        new THREE.MeshNormalMaterial()
+        new THREE.MeshNormalMaterial(),
+        new THREE.MeshStandardMaterial({color: 0x2ecc71, roughness: 0}),
+        new THREE.MeshStandardMaterial({color: 0xf1c40f, roughness: 0.4}),
+        new THREE.MeshStandardMaterial({color: 0xe74c3c, roughness: 0.1}),
+        new THREE.MeshStandardMaterial({color: 0x8e44ad, roughness: 0.1}),
+        new THREE.MeshStandardMaterial({color: 0x1abc9c, roughness: 0.1}),
+        new THREE.MeshStandardMaterial({color: 0x2980b9, roughness: 0, metalness: 0.5}),
+        new THREE.MeshStandardMaterial({color: 0x2c3e50, roughness: 0, metalness: 0.5}),
     ]
     return geometries.map(({position,r,geometry})=> (
         <Geometry key={JSON.stringify(position)} position={position.map((p) => p*2)} geometry={geometry} materials={materials} r={r} />
