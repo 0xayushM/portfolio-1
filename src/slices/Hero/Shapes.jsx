4bf1c852fb1605bfb671eb753362941aeeed02ff
Ayush Mangal <mangal.ayush.4982@gmail.com>
2024-02-02 21:07:20 +0530

    added sound effects

--- src/slices/Hero/Shapes.jsx
+++ src/slices/Hero/Shapes.jsx
@@ -25,27 +25,27 @@ function Geometries() {
         {
             position: [0,0,0],
             r:0.3,
-            geometry: new THREE.IcosahedronGeometry(3) 
+            geometry: new THREE.IcosahedronGeometry(3) // Gem
         }, 
         {
             position: [1,-0.75,4],
             r:0.4 ,
-            geometry: new THREE.CapsuleGeometry(0.5,1.6,2,16) 
+            geometry: new THREE.CapsuleGeometry(0.5,1.6,2,16) // Pill 
         }, 
         {
             position: [-1.4,2,-4],
             r:0.6,
-            geometry: new THREE.DodecahedronGeometry(1.5) 
+            geometry: new THREE.DodecahedronGeometry(1.5)  // Ball
         }, 
         {
             position: [-0.8,-0.75,5],
             r:0.3,
-            geometry: new THREE.TorusGeometry(0.6,0.25,16,25) 
+            geometry: new THREE.TorusGeometry(0.6,0.25,16,25) // Donut
         }, 
         {
             position: [1.6,1.6,-4],
             r:0.7,
-            geometry: new THREE.OctahedronGeometry(1.5) 
+            geometry: new THREE.OctahedronGeometry(1.5) // Diamond
         }, 
     ]
     const materials = [
@@ -58,12 +58,26 @@ function Geometries() {
         new THREE.MeshStandardMaterial({color: 0x2980b9, roughness: 0, metalness: 0.5}),
         new THREE.MeshStandardMaterial({color: 0x2c3e50, roughness: 0, metalness: 0.5}),
     ]
+
+    const soundEffects = [
+        new Audio('/sounds/knock1.ogg'),
+        new Audio('/sounds/knock2.ogg'),
+        new Audio('/sounds/knock3.ogg'),
+    ]
+
     return geometries.map(({position,r,geometry})=> (
-        <Geometry key={JSON.stringify(position)} position={position.map((p) => p*2)} geometry={geometry} materials={materials} r={r} />
+        <Geometry 
+            key={JSON.stringify(position)} 
+            position={position.map((p) => p*2)} 
+            geometry={geometry} 
+            materials={materials} 
+            r={r} 
+            soundEffects={soundEffects}
+        />
     ))
 }
 
-function Geometry({r,position, geometry, materials}) {
+function Geometry({r,position, geometry, soundEffects, materials}) {
     const meshRef = useRef()
     const [visible, setVisible] = useState(false)
 
@@ -74,6 +88,8 @@ function Geometry({r,position, geometry, materials}) {
     }
     function handleClick(e) {
         const mesh = e.object;
+
+        gsap.utils.random(soundEffects).play()
 
         gsap.to(mesh.rotation, {
             x:`+=${gsap.utils.random(0,2)}`,
