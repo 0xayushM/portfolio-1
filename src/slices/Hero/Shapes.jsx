3e8d0742bf089f12befcd218e98002b7cb3b0493
Ayush Mangal <mangal.ayush.4982@gmail.com>
2024-02-02 20:57:06 +0530

    added more geometries

--- src/slices/Hero/Shapes.jsx
+++ src/slices/Hero/Shapes.jsx
@@ -26,7 +26,27 @@ function Geometries() {
             position: [0,0,0],
             r:0.3,
             geometry: new THREE.IcosahedronGeometry(3) 
-        },
+        }, 
+        {
+            position: [1,-0.75,4],
+            r:0.4 ,
+            geometry: new THREE.CapsuleGeometry(0.5,1.6,2,16) 
+        }, 
+        {
+            position: [-1.4,2,-4],
+            r:0.6,
+            geometry: new THREE.DodecahedronGeometry(1.5) 
+        }, 
+        {
+            position: [-0.8,-0.75,5],
+            r:0.3,
+            geometry: new THREE.TorusGeometry(0.6,0.25,16,25) 
+        }, 
+        {
+            position: [1.6,1.6,-4],
+            r:0.7,
+            geometry: new THREE.OctahedronGeometry(1.5) 
+        }, 
     ]
     const materials = [
         new THREE.MeshNormalMaterial(),
