0339acdf579370ace29a4698d4b1ef4092ee31a0
Ayush Mangal <mangal.ayush.4982@gmail.com>
2024-02-03 18:21:36 +0530

    added gsap animation to avatar

--- src/slices/Biography/Avatar.tsx
+++ src/slices/Biography/Avatar.tsx
@@ -2,7 +2,8 @@
 import { ImageField } from "@prismicio/client";
 import { PrismicNextImage } from "@prismicio/next";
 import clsx from "clsx";
-import { useRef } from "react";
+import {gsap} from "gsap";
+import { useEffect, useRef } from "react";
 
 type AvatarProps = {
   image: ImageField;
@@ -11,15 +12,26 @@ type AvatarProps = {
 
 export default function Avatar({ image, className }: AvatarProps) {
     const component = useRef(null)
+
+    useEffect(()=> {
+        let ctx = gsap.context(()=>{
+            gsap.fromTo (
+                '.avatar',
+                {opacity:0, scale:1.4},
+                {opacity:1, scale:1, duration:1.3, ease: "power3.inOut"},
+            )
+        })
+    })
   
     return (
     <div ref={component} className={clsx("relative h-full w-full", className)}>
-      <div className="avatar overflow-hidden rounded-3xl border-2 border-slate-700 opacity-0f">
+      <div className="avatar overflow-hidden rounded-3xl border-2 border-slate-700 opacity-0">
         <PrismicNextImage
           field={image}
           className="avatar-image h-full w-full object-fill"
           imgixParams={{q:90}}
         />
+        <div className="highlight absolute inset-0 hidden w-full scale-110 bg-gradient-to-tr from-transparent via-white to-transparent opacity-0 md:block"></div>
       </div>
     </div>
   );
