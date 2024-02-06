6e4409f03a01a199a5223b23c8127d5423ffb586
Ayush Mangal <mangal.ayush.4982@gmail.com>
2024-02-07 01:10:46 +0530

    done with blog and projects preview page

--- src/slices/index.ts
+++ src/slices/index.ts
@@ -5,7 +5,9 @@ import dynamic from "next/dynamic";
 export const components = {
   biography: dynamic(() => import("./Biography")),
   content_index: dynamic(() => import("./ContentIndex")),
+  experience: dynamic(() => import("./Experience")),
   hero: dynamic(() => import("./Hero")),
+  image_block: dynamic(() => import("./ImageBlock")),
   settings: dynamic(() => import("./Settings")),
   tech_list: dynamic(() => import("./TechList")),
   text_block: dynamic(() => import("./TextBlock")),
